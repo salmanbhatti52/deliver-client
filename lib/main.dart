4e11027a59ab44664c9b831d725668133792027f
Areeb Rajwana <areebrajwana@gmail.com>
2023-09-30 14:00:16 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:deliver_client/utils/colors.dart';
 import 'package:deliver_client/screens/splash_screen.dart';
+import 'package:deliver_client/utils/firebase_options.dart';
 import 'package:deliver_client/utils/remove_scroll_glow.dart';
-
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
