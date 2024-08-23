afdd06907095828568bbd7731cb85510c5a09b7e
Salman Ahmad Bhatti <salmanbhatti52@hotmail.com>
2024-08-23 17:00:15 +0500

    ios update

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\android studio\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\flutter-Project\Office\deliver-client"
+export "FLUTTER_ROOT=/Users/salmanbhatti52/documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/salmanbhatti52/Desktop/deliver-client"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.3.2"
-export "FLUTTER_BUILD_NUMBER=200"
+export "FLUTTER_BUILD_NAME=1.3.3"
+export "FLUTTER_BUILD_NUMBER=300"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
