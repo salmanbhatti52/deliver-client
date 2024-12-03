1c7c49fdbaa1cc3377fe1c5170bc19e9f42a1299
Hammad Siddiqui <hammadraza5388@gmail.com>
2024-12-03 11:46:45 +0500

    version update

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/salmanbhatti52/documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/salmanbhatti52/Desktop/deliver-client"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter\flutter_projects\GitHubOffice\deliver-client"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.3.9"
-export "FLUTTER_BUILD_NUMBER=900"
+export "FLUTTER_BUILD_NAME=1.3.11"
+export "FLUTTER_BUILD_NUMBER=902"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
